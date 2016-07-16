dbc5882773b2770352edf1c4d2ac0cba55363fe5
Ram Kaniyur <quadrupleslap@gmail.com>
2016-07-16 15:47:15 +1000

    Finished an unnecessarily complicated Caesar Cipher demo.

--- Ciphers/Pages/Caesar/Demo.xaml.vb
+++ Ciphers/Pages/Caesar/Demo.xaml.vb
@@ -1,3 +1,100 @@
 ﻿Class CaesarDemo
+    Private Modding As Boolean = False
 
+    Sub New()
+        InitializeComponent()
+
+        For i = 0 To UPPER.Length - 1
+            Dim plain = New Label With {.Content = UPPER(i)}
+            Grid.SetColumn(plain, i)
+            PlainGrid.Children.Add(plain)
+
+            PlainGrid.ColumnDefinitions.Add(New ColumnDefinition With {
+                .Width = New GridLength(1, GridUnitType.Star)
+            })
+        Next
+
+        ComponentModel.DependencyPropertyDescriptor.
+            FromProperty(ActualWidthProperty, GetType(Grid)).
+            AddValueChanged(Naruto, AddressOf Hoi)
+
+        TryShift()
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        ComponentModel.DependencyPropertyDescriptor.
+            FromProperty(ActualWidthProperty, GetType(Grid)).
+            RemoveValueChanged(Naruto, AddressOf Hoi)
+        MyBase.Finalize()
+    End Sub
+
+    Sub MoveCursor(c As Char)
+        If UPPER.Contains(c) Then
+            TableCursor.BeginAnimation(
+                MarginProperty,
+                New Animation.ThicknessAnimation(
+                    New Thickness(
+                        151 + UPPER.IndexOf(c) * Naruto.ActualWidth / UPPER.Length,
+                        81.39, 0, 0),
+                    New Duration(TimeSpan.FromSeconds(0.15)),
+                    Animation.FillBehavior.HoldEnd),
+                Animation.HandoffBehavior.SnapshotAndReplace)
+            TableCursor.Background.BeginAnimation(
+                TileBrush.ViewboxProperty,
+                New Animation.RectAnimation(
+                    New Rect(UPPER.IndexOf(c) / UPPER.Length, 0.0, 1.0, 1.0),
+                    New Duration(TimeSpan.FromSeconds(0.15)),
+                    Animation.FillBehavior.HoldEnd),
+                Animation.HandoffBehavior.SnapshotAndReplace)
+        End If
+    End Sub
+
+    Sub TryShift()
+        If Not IsInitialized Then Return
+
+        GridPan.BeginAnimation(
+            TileBrush.ViewportProperty,
+            New Animation.RectAnimation(
+                New Rect(-ShiftBox.Value / UPPER.Length, 0.0, 1.0, 1.0),
+                New Duration(TimeSpan.FromSeconds(0.25)),
+                Animation.FillBehavior.HoldEnd),
+            Animation.HandoffBehavior.SnapshotAndReplace)
+    End Sub
+
+    Sub Hoi()
+        TableCursor.Width = Naruto.ActualWidth / UPPER.Length
+    End Sub
+
+    Sub ShiftChanged() Handles ShiftBox.ValueChanged
+        TryShift()
+        UpdateCiphered()
+    End Sub
+
+    Sub UpdateCiphered() Handles PlainBox.TextChanged
+        If Not Modding Then
+            Modding = True
+            CipheredBox.Text = CaesarEncode(ShiftBox.Value, PlainBox.Text)
+            Modding = False
+        End If
+    End Sub
+
+    Sub UpdatePlain() Handles CipheredBox.TextChanged
+        If Not Modding Then
+            Modding = True
+            PlainBox.Text = CaesarDecode(ShiftBox.Value, CipheredBox.Text)
+            Modding = False
+        End If
+    End Sub
+
+    Private Sub PlainInput(sender As Object, e As KeyEventArgs) Handles PlainBox.KeyDown
+        If KEYS.Contains(e.Key) Then
+            MoveCursor(UPPER(Array.IndexOf(KEYS, e.Key)))
+        End If
+    End Sub
+
+    Private Sub EncodedInput(sender As Object, e As KeyEventArgs) Handles CipheredBox.KeyDown
+        If KEYS.Contains(e.Key) Then
+            MoveCursor(CaesarDecode(ShiftBox.Value, UPPER(Array.IndexOf(KEYS, e.Key))))
+        End If
+    End Sub
 End Class
---
+++ Ciphers/Utilities/Encoding.vb
@@ -0,0 +1,35 @@
+﻿Public Module Encoding
+    Public LOWER As String = "abcdefghijklmnopqrstuvwxyz"
+    Public UPPER As String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    Public KEYS() As Key = {
+        Key.A, Key.B, Key.C, Key.D, Key.E, Key.F, Key.G, Key.H, Key.I, Key.J, Key.K, Key.L, Key.M,
+        Key.N, Key.O, Key.P, Key.Q, Key.R, Key.S, Key.T, Key.U, Key.V, Key.W, Key.X, Key.Y, Key.Z
+    }
+
+    Dim encoded
+    Public Function CaesarEncode(rot As Integer, inp As String)
+        rot = (rot Mod UPPER.Length + UPPER.Length) Mod UPPER.Length
+        Dim out(inp.Length - 1) As Char
+
+        For i = 0 To inp.Length - 1
+            Dim c = inp(i)
+            If LOWER.Contains(c) Then
+                Dim original = LOWER.IndexOf(c)
+                Dim encoded = (original + rot) Mod LOWER.Length
+                out(i) = LOWER(encoded)
+            ElseIf UPPER.Contains(c) Then
+                Dim original = UPPER.IndexOf(c)
+                Dim encoded = (original + rot) Mod UPPER.Length
+                out(i) = UPPER(encoded)
+            Else
+                out(i) = c
+            End If
+        Next i
+
+        Return New String(out)
+    End Function
+
+    Public Function CaesarDecode(rot As Integer, inp As String)
+        Return CaesarEncode(-rot, inp)
+    End Function
+End Module
