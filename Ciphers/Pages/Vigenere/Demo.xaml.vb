4674dbbb6924a1f6f1c5fbc1bb6caac852544ea2
Ram Kaniyur <quadrupleslap@gmail.com>
2016-08-02 14:59:38 +1000

    A bunch of stuff.

--- Ciphers/Pages/Vigenere/Demo.xaml.vb
+++ Ciphers/Pages/Vigenere/Demo.xaml.vb
@@ -1,3 +1,45 @@
 ﻿Class VigenereDemo
+    Private Modding As Boolean = False
 
+    Sub Ready() Handles Me.Loaded
+        CalcTutorialButton()
+    End Sub
+
+    Sub CalcTutorialButton()
+        If PlainBox.Text.Length = 0 Or KeyBox.Text.Length = 0 Then
+            TutorialButton.IsEnabled = False
+        Else
+            TutorialButton.IsEnabled = True
+        End If
+    End Sub
+
+    Sub KeyChanged() Handles KeyBox.TextChanged
+        UpdateCiphered()
+    End Sub
+
+    Sub UpdateCiphered() Handles PlainBox.TextChanged
+        If Not Modding Then
+            Modding = True
+            CipheredBox.Text = VigenereEncode(KeyBox.Text, PlainBox.Text)
+            Modding = False
+        End If
+        CalcTutorialButton()
+    End Sub
+
+    Sub UpdatePlain() Handles CipheredBox.TextChanged
+        If Not Modding Then
+            Modding = True
+            PlainBox.Text = VigenereDecode(KeyBox.Text, CipheredBox.Text)
+            Modding = False
+        End If
+        CalcTutorialButton()
+    End Sub
+
+    Sub ShowTutorial() Handles TutorialButton.Click
+        Dim win = New Modal(Window.GetWindow(Me), New Tutorial(KeyBox.Text, PlainBox.Text))
+        win.Width = Window.GetWindow(Me).Width * 0.8
+        win.Height = Window.GetWindow(Me).Height * 0.8
+        win.Title = "Tutorial"
+        win.ShowDialog()
+    End Sub
 End Class
--- Ciphers/Utilities/Encoding.vb
+++ Ciphers/Utilities/Encoding.vb
@@ -7,7 +7,7 @@
     }
 
     Dim encoded
-    Public Function CaesarEncode(rot As Integer, inp As String)
+    Public Function CaesarEncode(rot As Integer, inp As String) As String
         rot = (rot Mod UPPER.Length + UPPER.Length) Mod UPPER.Length
         Dim out(inp.Length - 1) As Char
 
@@ -29,7 +29,128 @@
         Return New String(out)
     End Function
 
-    Public Function CaesarDecode(rot As Integer, inp As String)
+    Public Function CaesarDecode(rot As Integer, inp As String) As String
         Return CaesarEncode(-rot, inp)
+    End Function
+
+    Public Function VigenereEncode(keyStr As String, inp As String) As String
+        If keyStr.Length = 0 Then Return inp
+
+        keyStr = keyStr.ToUpper
+        Dim out(inp.Length - 1) As Char
+
+        Dim key As New List(Of Char)
+        For i = 0 To keyStr.Length - 1
+            If UPPER.Contains(keyStr(i)) Then
+                key.Add(keyStr(i))
+            End If
+        Next i
+
+        For i = 0 To inp.Length - 1
+            Dim c = inp(i)
+            If UPPER.Contains(c) Then
+                Dim rot = UPPER.IndexOf(key(i Mod key.Count))
+                Dim original = UPPER.IndexOf(c)
+                Dim encoded = (original + rot) Mod UPPER.Length
+                out(i) = UPPER(encoded)
+            ElseIf LOWER.Contains(c) Then
+                Dim rot = UPPER.IndexOf(key(i Mod key.Count))
+                Dim original = LOWER.IndexOf(c)
+                Dim encoded = (original + rot) Mod UPPER.Length
+                out(i) = LOWER(encoded)
+            Else
+                out(i) = c
+            End If
+        Next i
+
+        Return New String(out)
+    End Function
+
+    Public Function VigenereDecode(keyStr As String, inp As String) As String
+        If keyStr.Length = 0 Then Return inp
+
+        keyStr = keyStr.ToUpper
+        Dim out(inp.Length - 1) As Char
+
+        Dim key As New List(Of Char)
+        For i = 0 To keyStr.Length - 1
+            If UPPER.Contains(keyStr(i)) Then
+                key.Add(keyStr(i))
+            End If
+        Next i
+
+        For i = 0 To inp.Length - 1
+            Dim c = inp(i)
+            If UPPER.Contains(c) Then
+                Dim rot = UPPER.IndexOf(key(i Mod key.Count))
+                Dim original = UPPER.IndexOf(c)
+                Dim encoded = (original + UPPER.Length - rot) Mod UPPER.Length
+                out(i) = UPPER(encoded)
+            ElseIf LOWER.Contains(c) Then
+                Dim rot = UPPER.IndexOf(key(i Mod key.Count))
+                Dim original = LOWER.IndexOf(c)
+                Dim encoded = (original + UPPER.Length - rot) Mod UPPER.Length
+                out(i) = LOWER(encoded)
+            Else
+                out(i) = c
+            End If
+        Next i
+
+        Return New String(out)
+    End Function
+
+    Public Function StraddlingEncode(keyStr As String, n1 As Integer, n2 As Integer, inp As String) As String
+        Debug.Assert(0 <= n1 < 10 And 0 <= n2 < 10 And n1 <> n2)
+
+        keyStr = keyStr.ToUpper
+        inp = inp.ToUpper
+        If n2 > n1 Then
+            Dim swp = n1
+            n1 = n2
+            n2 = swp
+        End If
+
+        Dim key As New List(Of Char)
+        For i = 0 To keyStr.Length - 1
+            If UPPER.Contains(keyStr(i)) And Not key.Contains(keyStr(i)) Then
+                key.Add(keyStr(i))
+            End If
+        Next i
+
+        For i = 0 To UPPER.Length - 1
+            If Not key.Contains(UPPER(i)) Then
+                key.Add(UPPER(i))
+            End If
+        Next i
+
+        Dim table As New Hashtable
+        For i = 0 To key.Count - 1
+            If i < n1 Then
+                table(key(i)) = i
+            ElseIf i < n2 - 1 Then
+                table(key(i)) = i + 1
+            ElseIf i < 8 Then
+                table(key(i)) = i + 2
+            ElseIf i < 18 Then
+                table(key(i)) = n1 * 10 + i - 8
+            ElseIf i < 28 Then
+                table(key(i)) = n2 * 10 + i - 18
+            Else
+                Throw New Exception("Unreachable!")
+            End If
+        Next
+
+        Dim out As New List(Of Char)
+        For i = 0 To inp.Length - 1
+            If table.ContainsKey(inp(i)) Then
+                out.Add(table(inp(i)))
+            End If
+        Next
+
+        Return New String(out.ToArray)
+    End Function
+
+    Public Function StraddlingDecode(key As String, keyN1 As Integer, keyN2 As Integer, inp As String) As String
+        Throw New NotImplementedException
     End Function
 End Module
